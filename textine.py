b5b18a714a69e700591e63ee6cbeedda9b399661
Sumit Ghosh <sumit.ghosh32@gmail.com>
2017-12-08 04:46:46 +0530

    Create create_graph function

--- textine.py
+++ textine.py
@@ -18,9 +18,21 @@ def decontract(text):
 		text = re.sub(r[0], r[1], text)
 	return text
 
-def preprocess(raw_text):
-	tokens = nltk.word_tokenize(raw_text)
+def preprocess(raw):
+	tokens = nltk.word_tokenize(raw)
 	return tokens
+
+def create_graph(tokens, dist):
+	graph = nx.Graph()
+
+	for index, token in enumerate(tokens):
+		for i in range(dist):
+			try:
+				graph.add_edge(token, tokens[index+i])
+			except IndexError:
+				pass
+
+	return graph
 
 with open('input.txt', 'r') as f:
 	raw = f.read()
